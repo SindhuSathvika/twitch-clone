c45a2abfe33aff20340d4beea080c4a4d383e81a
Sindhu Sathvika <sindhusathvikan@gmail.com>
2025-04-02 12:18:57 +0530

    -

---
+++ frontend1/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex justify-center items-center">
+      <h1 className="text-3xl font-bold">Welcome to Twitch Clone</h1>
+    </div>
+  );
+}
